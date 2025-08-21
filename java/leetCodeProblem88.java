332c1c86e2d86dbcea4368429d31f69272cce9b9
Prashans Sharma <89928763+Prashans11@users.noreply.github.com>
2025-08-21 23:58:29 +0530

    Java Solution for merging two sorted array into into a single array sorted in non-decreasing order.

---
+++ java/leetCodeProblem88.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        if(nums1.length < n){
+        return;
+        }
+
+        int i = m - 1;       
+        int j = n - 1;       
+        int k = m + n - 1;   
+
+        for (; i >= 0 && j >= 0; k--) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+        }
+        for (; j >= 0; j--, k--) {
+            nums1[k] = nums2[j];
+       }
+    }
+}
